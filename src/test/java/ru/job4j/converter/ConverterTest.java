58311b2a3f0a6caab3171db7cd03bd2f3df0893a
GANZO9055 <kosta.maslihov@gmail.com>
2023-10-11 15:11:43 +0300

    Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
-
     @Test
     void whenConvert140RblThen2Euro() {
         float in = 140;
