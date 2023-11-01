57c6a3f223e3181970b0495e93b7f337981e24d7
GANZO9055 <kosta.maslihov@gmail.com>
2023-11-01 20:32:32 +0300

    Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,4 +4,15 @@ public class Max {
     public static int max(int left, int right) {
         return left >= right ? left : right;
     }
+
+    public static int max(int left, int right, int one) {
+        return left >= right ? (left >= one ? left : one)
+                : (right >= one ? right : one);
+    }
+
+    public static int max(int left, int right, int one, int two) {
+        int zero = one >= two ? one : two;
+        return left >= right ? (left >= zero ? left : zero)
+                : (right >= zero ? right : zero);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -15,19 +15,22 @@ class MaxTest {
     }
 
     @Test
-    void whenMax4To1Then4() {
+    void whenMax4To1And3Then6() {
         int left = 4;
         int right = 1;
-        int result = Max.max(left, right);
-        int expected = 4;
+        int one = 6;
+        int result = Max.max(left, right, one);
+        int expected = 6;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax7To7Then7() {
+    void whenMax7To7And6To2Then7() {
         int left = 7;
         int right = 7;
-        int result = Max.max(left, right);
+        int one = 6;
+        int two = 2;
+        int result = Max.max(left, right, one, two);
         int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
