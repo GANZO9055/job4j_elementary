9d75db289fa7dc2bf1674c97f50688924c535871
GANZO9055 <kosta.maslihov@gmail.com>
2024-04-17 16:22:35 +0300

    Codium. Нейросети спешат на помощь. [#505152]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,21 +1,25 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
+    private static final short BASE_HEIGHT_MAN = 100;
+    private static final short BASE_HEIGHT_WOMAN = 110;
+    private static final double WEIGHT_COEFFICIENT = 1.15;
+
+    public static double calculateManWeight(int height) {
+        double rsl = (height - BASE_HEIGHT_MAN) * WEIGHT_COEFFICIENT;
         return rsl;
     }
 
-    public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
+    public static double calculateWomanWeight(int height) {
+        double rsl = (height - BASE_HEIGHT_WOMAN) * WEIGHT_COEFFICIENT;
         return rsl;
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
-        System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        int height = 187;
+        double man = Fit.calculateManWeight(height);
+        double woman = Fit.calculateWomanWeight(height);
+        System.out.printf("Man 187 is %.2f \n", man);
+        System.out.printf("Woman 187 is %.2f \n", woman);
     }
 }
